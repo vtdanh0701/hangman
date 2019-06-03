0ef6cd0509216c72b395aab27fcbc99eb0e57721
Danh Vuong <danhvuong@Danhs-Macbook.local>
2019-06-03 12:26:36 -0700

    add hangman img, fine tune

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 // Variables declarations
 var wordBank = ["blue","red","orange","shark","arpegios","milestone","flowers","bumblebee"];
 var underScore = [];
-var maxLives = 5; 
-var remainingLives;
+//var maxLives = 5; 
+var remainingLives = 5;
 var guessWord = [];
 var guessedLetter =[];
 var endGame = false; 
 var gameStart = false;
 var win = false;
+document.getElementById("startbut").style.visibility = "hidden";
 
 
 // function 
 function reset(){
+    document.getElementById("startbut").style.visibility = "visible";
+
     endGame = false;
     win = false;
     gameStart = true;
-    remainingLives = maxLives;
+    hidden = "visible";
+    //remainingLives = maxLives;
     guessedLetter = [];
     guessWord = [];
     underScore = [];
@@ -23,29 +27,34 @@ function reset(){
     for(var i = 0; i < guessWord.length; i++){
         underScore.push("_");
     }
-    document.getElementById("startBut").textContent = "Restart";
-    document.getElementById("hiddenMessage2").textContent = " ";
+    document.getElementById("beginprompt").textContent = "Guess the letter below";
+    button = document.getElementById("startbut");
+    button.textContent = "Restart";
+    
+    document.getElementById("hiddenmessage2").textContent = " ";
     document.getElementById("intro").textContent = "Please enter a letter to guess"
     updateDisplay();
+    
 }
 function updateDisplay(){
-    document.getElementById("underScore").textContent = underScore.join(" "); 
+    document.getElementById("underscore").textContent = underScore.join(" "); 
     document.getElementById("lives").textContent = remainingLives; 
-    document.getElementById("hiddenMessage").textContent = " ";
-    document.getElementById("inputLetter").value = ""; 
+    document.getElementById("hiddenmessage").textContent = " ";
+    document.getElementById("inputletter").value = ""; 
+    document.getElementById("hangmanpic").src = "assets/" + remainingLives + ".png";
     
 }
 
 function makeGuess(inputLetter){
-    var inputRaw = document.getElementById("inputLetter").value;
+    var inputRaw = document.getElementById("inputletter").value;
     var inputLetter = inputRaw.toLowerCase();
     if(guessWord.length === 0){
-        document.getElementById("hiddenMessage2").textContent = "Please click button to begin";
+        document.getElementById("hiddenmessage2").textContent = "Please click button to begin";
 
     }
     if(remainingLives > 0 ){
         if(/[^a-zA-Z\-\/]/.test(inputLetter)){
-            document.getElementById("hiddenMessage2").textContent = "Not a letter, try again";
+            document.getElementById("hiddenmessage2").textContent = "Not a letter, try again";
 
         }
         
@@ -55,25 +64,25 @@ function makeGuess(inputLetter){
     
                 if(inputLetter === guessWord[i]){
                     underScore[i] = inputLetter;
-                    document.getElementById("hiddenMessage2").textContent = "Well Done!! ";
+                    document.getElementById("hiddenmessage2").textContent = "Well Done!! ";
                 }
                 
             }
             if(guessWord.indexOf(inputLetter) === -1){
                 remainingLives -= 1;
-                document.getElementById("hiddenMessage2").textContent = "Sorry, try again";
+                document.getElementById("hiddenmessage2").textContent = "Sorry, try again";
             }
         }      
         else if(inputLetter.length > 1){
-            document.getElementById("hiddenMessage2").textContent = "Please enter only 1 letter. ";
+            document.getElementById("hiddenmessage2").textContent = "Please enter only 1 letter. ";
         }
         else if(inputLetter.length <1){
-            document.getElementById("hiddenMessage2").textContent = "Please enter a letter. ";
+            document.getElementById("hiddenmessage2").textContent = "Please enter a letter. ";
 
         }
         
         else{
-            document.getElementById("hiddenMessage2").textContent = "You already typed this in, try something else. ";
+            document.getElementById("hiddenmessage2").textContent = "You already typed this in, try something else. ";
         }
     } 
    
@@ -85,23 +94,19 @@ function makeGuess(inputLetter){
 function checkWin(){
     if(underScore.indexOf("_") === -1 && guessedLetter.length > 0){
         win = true;
-        document.getElementById("hiddenMessage").textContent = "Congratulation ! You win !!"
-        document.getElementById("hiddenMessage2").textContent = " ";
+        document.getElementById("hiddenmessage").textContent = "Congratulation ! You win !!"
+        document.getElementById("hiddenmessage2").textContent = " ";
 
     }
 }
 function checkLoss(){
     if(remainingLives === 0){
-        document.getElementById("hiddenMessage").textContent = "You lose, try again :("
-        document.getElementById("hiddenMessage2").textContent = " ";
+        document.getElementById("hiddenmessage").textContent = "You lose, try again :("
+        document.getElementById("hiddenmessage2").textContent = " ";
 
     }
 }
 
-function addElement(){
-    var button = document.createElement("button")
-    var text = document.createTextNode("Guess")
-    button.appendChild(text);
-    var main = document.getElementsByTagName("main");
-    main.appendChild("button");
+
+function hideButton(){
 }
